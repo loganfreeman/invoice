cecf637bb7dcd77657f31edae4d33ab6d7d6fab1
shanhong cheng <shanhong.cheng@utah.edu>
2016-04-08 15:08:03 -0600

    new park

--- resources/views/parks/edit.blade.php
+++ resources/views/parks/edit.blade.php
@@ -1,1 +1,33 @@
 @extends('header')
+
+@section('onReady')
+	$('input#name').focus();
+@stop
+
+@section('content')
+
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<div class="row">
+
+  {!! Former::open($url)
+            ->autocomplete('off')
+            ->rules(
+                []
+            )->addClass('col-md-12 warn-on-exit')
+            ->method($method) !!}
+
+    @include('partials.autocomplete_fix')
+
+  {!! Former::close() !!}
+</div>
+
+@stop
